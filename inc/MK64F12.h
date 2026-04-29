b71ab5c3139f70bc28a98f4f957e64533dd4cba8
ut2or1 <48625958+ut2or1@users.noreply.github.com>
2026-04-29 12:25:56 +0300

    Update MK64F12.h

--- inc/MK64F12.h
+++ inc/MK64F12.h
@@ -3,37 +3,28 @@
 
 #include <stdint.h>
 
-// Базовые адреса
 #define SIM_BASE       0x40047000u
-#define PORTC_BASE     0x400FF080u
 
-// System Integration Module
 typedef struct {
     uint32_t SOPT1;
     uint32_t SOPT1CFG;
-    uint32_t RESERVED0[1022];
+    uint32_t RESERVED0[2];
+    uint32_t SOPT2;
+    uint32_t RESERVED1[3];
+    uint32_t SCGC4;
     uint32_t SCGC5;
+    uint32_t SCGC6;
+    uint32_t SCGC7;
+    uint32_t CLKDIV1;
+    uint32_t CLKDIV2;
+    uint32_t FCFG1;
+    uint32_t FCFG2;
+    uint32_t UIDH;
+    uint32_t UIDMH;
+    uint32_t UIDML;
+    uint32_t UIDL;
 } SIM_Type;
 
 #define SIM ((SIM_Type *)SIM_BASE)
-#define SIM_SCGC5_PORTC_MASK    (1 << 11)
 
-// Port Control
-typedef struct {
-    uint32_t PCR[32];
-    uint32_t GPCLR;
-    uint32_t GPCHR;
-    uint32_t RESERVED0[6];
-    uint32_t ISFR;
-    uint32_t RESERVED1[7];
-    uint32_t PDOR;
-    uint32_t PSOR;
-    uint32_t PCOR;
-    uint32_t PTOR;
-    uint32_t PDIR;
-    uint32_t PDDR;
-} PORT_Type;
-
-#define PORTC ((PORT_Type *)PORTC_BASE)
-
-#endif // MK64F12_H
+#endif
