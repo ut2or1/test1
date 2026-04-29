be5da36ed413ed13fed864b78379fb4691f9e20c
ut2or1 <48625958+ut2or1@users.noreply.github.com>
2026-04-29 12:26:15 +0300

    Update startup.c

--- src/startup.c
+++ src/startup.c
@@ -1,25 +1,19 @@
 #include <stdint.h>
 
 extern int main(void);
+
 extern uint32_t _etext, _sdata, _edata, _sbss, _ebss;
 
-// cppcheck-suppress constParameterPointer
 void Reset_Handler(void) {
-    const uint32_t *src = &_etext;
-    // cppcheck-suppress comparePointers
-    uint32_t *dst = (uint32_t *)&_sdata;
+    uint32_t *src = &_etext;
+    uint32_t *dst = &_sdata;
     
-    // Копируем data section из Flash в RAM
-    // cppcheck-suppress comparePointers
-    while ((uint32_t)dst < (uint32_t)&_edata) {
+    while (dst < &_edata) {
         *dst++ = *src++;
     }
     
-    // Обнуляем bss
-    // cppcheck-suppress comparePointers
-    dst = (uint32_t *)&_sbss;
-    // cppcheck-suppress comparePointers
-    while ((uint32_t)dst < (uint32_t)&_ebss) {
+    dst = &_sbss;
+    while (dst < &_ebss) {
         *dst++ = 0;
     }
     
@@ -32,20 +26,19 @@ void Default_Handler(void) {
     while (1);
 }
 
-// Таблица векторов прерываний
 __attribute__((section(".vectors")))
 void (* const vector_table[])(void) = {
-    (void (*)(void))0x20000000,  // Начальное значение SP
+    (void (*)(void))0x20000000,
     Reset_Handler,
-    Default_Handler,  // NMI
-    Default_Handler,  // HardFault
-    Default_Handler,  // MemManage
-    Default_Handler,  // BusFault
-    Default_Handler,  // UsageFault
-    0, 0, 0, 0,       // Reserved
-    Default_Handler,  // SVC
-    Default_Handler,  // DebugMon
-    0,                 // Reserved
-    Default_Handler,  // PendSV
-    Default_Handler,  // SysTick
+    Default_Handler,
+    Default_Handler,
+    Default_Handler,
+    Default_Handler,
+    Default_Handler,
+    0, 0, 0, 0,
+    Default_Handler,
+    Default_Handler,
+    0,
+    Default_Handler,
+    Default_Handler,
 };
